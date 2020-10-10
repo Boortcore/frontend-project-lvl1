38a9391bbcc354f7c4088b38411b834be4ab2225
Sergey Burtylev <sn.burtylev@gmail.com>
2020-10-10 12:55:57 +0300

    task 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+const greetUser = require('../src/cli.js');
+
+greetUser();
---
+++ src/cli.js
@@ -0,0 +1,8 @@
+const promptly = require('promptly');
+
+module.exports = async () => {
+  console.log('Welcome to the Brain Games!');
+  const name = await promptly.prompt('May I have your name?');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
